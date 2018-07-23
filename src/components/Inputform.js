ced0fa71a125358cd1fbdc3abad33d5a9f87ac04
Natalie <natalie.todd15@gmail.com>
2018-07-23 01:30:32 -0600

    added form info. w but lost the job info

---
+++ src/components/Inputform.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default class Inputform extends React.Component {
+    state = {
+
+    }
+
+    handleChange = (event) => {
+
+    }
+    handleSubmit = (event) => {
+
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='apply-here'>Apply Here:</label>
+                <input id='apply-box'
+                type='text'
+                value={this.state.title}
+                name='title'
+                onChange={this.handleChange} />
+                </form>
+        )
+    }
+}
