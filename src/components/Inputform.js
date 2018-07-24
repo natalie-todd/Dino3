efbed8d081b6910fedc9f533ad1cd180d6523154
Natalie <natalie.todd15@gmail.com>
2018-07-23 23:06:17 -0600

    changed input to textarea

--- src/components/Inputform.js
+++ src/components/Inputform.js
@@ -2,26 +2,26 @@ import React from 'react'
 
 export default class Inputform extends React.Component {
     state = {
-
+        shown: ['', 'Your application was submitted!']
     }
-
-    handleChange = (event) => {
-
-    }
+    
     handleSubmit = (event) => {
+        event.preventDefault()
 
     }
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label htmlFor='apply-here'>Apply Here:</label>
-                <input id='apply-box'
-                type='text'
-                value={this.state.title}
-                name='title'
-                onChange={this.handleChange} />
-                <input type='submit' name='submit' value='Submit' />
-                </form>
+                <textarea id='application-text'
+                    rows='8'
+                    cols='100'/>
+                    {/* value={this.state.title}
+                    name='title'
+                    onChange={this.handleChange}  */}
+                <input onClick={this.toggle} id='submit' type='submit' value='Submit' />
+            </form>
+
         )
     }
 }
