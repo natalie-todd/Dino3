dff82b5b5f771255482a530ce5a013bc9e7d3750
Natalie <natalie.todd15@gmail.com>
2018-07-23 23:06:56 -0600

    npm run build

--- src/components/Jobdetails.js
+++ src/components/Jobdetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Details({ data }) {
+export default function Jobdetails({ data }) {
   if (!data || data.length < 1) return <h3>No Jobs Found</h3>;
   console.log(data);
     return (
@@ -9,9 +9,9 @@ export default function Details({ data }) {
         data.map(data => (
           <li key={data.id}>
             <h4>{data.title}</h4>
-            <small>{data.pay}</small>
+            {/* <small>{data.pay}</small> */}
             <p>{data.description}</p>
-            <small>{`${data.interested.length} dinos are interested in this job`}</small>
+            {/* <small>{`${data.interested.length} dinos are interested in this job`}</small> */}
           </li>
         ))}
     </ul >
---
+++ src/components/Preview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//Change from class to same as Jobdetails if you don't use state
+export default class Preview extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <section id="application-preview">
+
+            </section>
+        )
+    }
+}
