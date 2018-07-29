b01673ce5769c9d8877eb10b9715190facb26189
Natalie <natalie.todd15@gmail.com>
2018-07-28 20:18:56 -0600

    added state and hide and show preview

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
-const Preview = ({ criteria }) => {
-    return (
-        <section id="application-preview" key={criteria}>{criteria}</section>
-    )
+class Preview extends Component {
+    state = {
+        hidden: true
+    }
+    showPreview = () => {
+        if (!this.state.hidden) {
+            return this.hidePreview()
+        } else {
+            return this.showPreview()
+        }
+    }
+    hidePreview = () => {
+        this.setState({ hidden: true })
+    }
+    showPreview = () => {
+        this.setState({ hidden: false })
+    }
+    render() {
+        return (
+            <div>
+                <button id='preview-toggle' className='preview'
+                    onClick={() => this.togglePreview()}>Preview</button>
+                {!this.state.hidden && <section id='application-preview'>{this.props.application.application}
+                </section>}
+            </div>
+        )
+    }
 }
+
 export default Preview;
